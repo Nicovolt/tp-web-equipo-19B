05ca5fb0a0915e88c4c1ef52f61ec04b8b4d030d
Nicovolt <166955943+Nicovolt@users.noreply.github.com>
2024-09-30 15:23:19 -0300

    comienzo
    agregue la cantidad de ventanas que vamos a usar, las clases, biblioteca de clases, etc.

--- tp-web-equipo-19B/tp-web-equipo-19B/Datos.aspx.cs
+++ tp-web-equipo-19B/tp-web-equipo-19B/Datos.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace tp_web_equipo_19B
 {
-    public partial class About : Page
+    public partial class Datos : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
