c9b6f19e63ca1c32b023d8d6340cea6a27303f42
Nicovolt <166955943+Nicovolt@users.noreply.github.com>
2024-10-04 10:52:38 -0300

    gmail
    cree la clase gmail y sus funciones para usar luego.

---
+++ tp-web-equipo-19B/Negocio/gmail.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class gmail
+    {
+        private SmtpClient server;
+        private MailMessage mail;
+
+        public gmail()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("5.3.pereznicolas@gmail.com", "xcib wejn uzkh efxh");
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "smtp.gmail.com";
+        }
+
+        public void armarCorreo(string destino, string asunto, string cuerpo)
+        {
+            mail = new MailMessage();
+            mail.From = new MailAddress("noresponder@tienda30.com");
+            mail.To.Add(destino);
+            mail.Subject = asunto;
+            mail.Body = cuerpo;
+        }
+
+        public void enviarMail()
+        {
+            try
+            {
+                server.Send(mail);
+            }
+            catch (Exception ex)
+            {
+          
+                throw new Exception("Error al enviar el correo: " + ex.Message, ex);
+            }
+        }
+
+        
+    }
+}
